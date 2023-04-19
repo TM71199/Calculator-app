07ddfcffea53666247d7e68389076cbbe47c1dc0
TM71199 <tejaswimattaparthi07@gmail.com>
2023-04-19 09:50:17 +0530

    calculatorappteju

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React,{useState} from 'react';
+import './App.css';
+
+function App() {
+  const [result,setResult] = useState('');
+
+  const clearHandler = () => {
+    setResult('');
+  };
+
+  const deleteHandler = () => {
+    setResult(result.slice(0,result.length - 1))
+  };
+
+  const calculateHandler = () =>{
+    try{
+      setResult(eval(result).toString() );
+    }catch(err){
+      setResult('Syntax Error')
+    }
+    };
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name))
+  };
+  return (
+   
+   <div className='container'>
+      <form>
+        <input className = 'current-operand output' type={'text'} value = {result}/>
+      </form>
+      <div className='buttons'>
+        <button className ='ac span-two' onClick={clearHandler}>AC</button>
+        <button onClick={deleteHandler}>DEL</button>
+
+        <button name = '/' onClick={handleClick}>&divide;</button>
+        <button name = '1' onClick={handleClick}>1</button>
+        <button name = '2' onClick={handleClick}>2</button>
+        <button name = '3' onClick={handleClick}>3</button>
+        <button name = '*' onClick={handleClick}>&times;</button>
+        <button name = '4' onClick={handleClick}>4</button>
+        <button name = '5' onClick={handleClick}>5</button>
+        <button name = '6' onClick={handleClick}>6</button>
+        <button name = '-' onClick={handleClick}>&minus;</button>
+        <button name = '7' onClick={handleClick}>7</button>
+        <button name = '8' onClick={handleClick}>8</button>
+        <button name = '9' onClick={handleClick}>9</button>
+        <button name = '+' onClick={handleClick}>+</button>
+        <button name = '.' onClick={handleClick}>.</button>
+        <button name = '0' onClick={handleClick}>0</button>
+        <button name = '=' className ='span-two' onClick={calculateHandler}>=</button>
+      
+      </div>
+   </div> 
+
+  );
+}
+
+export default App;
